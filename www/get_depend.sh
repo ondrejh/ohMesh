cb0a4ea4cfe7e4d5cee200fbc7aaa217f47316f3
Ondrej Hejda <ondrejh.ck@email.cz>
2025-05-17 18:42:57 +0200

    heltec v3 mobile build

---
+++ www/get_depend.sh
@@ -0,0 +1,3 @@
+# skriptik stahne soubory knihovny lightbox js a css do prilusnych adresaru
+wget -O style/lightbox.min.css https://raw.githubusercontent.com/lokesh/lightbox2/refs/heads/dev/dist/css/lightbox.min.css
+wget -O js/lightbox-plus-iquery.min.js https://raw.githubusercontent.com/lokesh/lightbox2/refs/heads/dev/dist/js/lightbox-plus-jquery.min.js
